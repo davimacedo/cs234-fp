5844d55d1054fabb6e22dee6649e31fd0c2553b1
Antonio Davi Macedo Coelho de Castro <adavimacedo@gmail.com>
2024-05-21 17:57:53 -0700

    Only gpt2

--- gpt2.py
+++ gpt2.py
@@ -1,8 +1,13 @@
 from transformers import pipeline
 
 def gpt2():
-    pipe = pipeline('text-generation', model='gpt2')
+    pipe = pipeline(
+        "text-generation",
+        model="gpt2-xl",
+        device_map="auto",
+        max_length=512
+    )
 
-    out = pipe("Hi!")
+    out = pipe("Q: What is the best in Soccer, Brazil or Argentina?\nA: \xa0The Best is the Brazilian side. \xa0They have everything, with fantastic players like Fredy, Carlos Alberto, Falcao, and Neymar!\nQ: Isn't Argentina better?\nA: ")
 
-    print(out[0])
+    print(out)
