fix log probablity calculation in evaluate

evaluate.py
@@ -16,28 +16,31 @@ def calculate_log_probs(model, tokenizer, input_token_ids_batch, min_input_lengt
 
     batch_token_ids = []
     indexes = []
+    input_token_ids_lengths = []
 
     for i, (input_token_ids, output_token_ids) in enumerate(zip(input_token_ids_batch, output_token_ids_batch)):
         token_ids = input_token_ids + output_token_ids + [tokenizer.eos_token_id]
         if len(token_ids) <= 1024:
             batch_token_ids.append(token_ids)
             indexes.append(i)
+            input_token_ids_lengths.append(len(input_token_ids))
         
     max_length = max(len(token_ids) for token_ids in batch_token_ids)
     padded_batch_token_ids = torch.tensor([token_ids + [tokenizer.eos_token_id] * (max_length - len(token_ids)) for token_ids in batch_token_ids], dtype=torch.long, device=device)
     
     attention_mask = torch.zeros(padded_batch_token_ids.shape, device=device)
+    probs_mask = torch.zeros(padded_batch_token_ids.shape, device=device)
     
     for i, attention_mask_row in enumerate(attention_mask):
         attention_mask_row[:len(batch_token_ids[i])] = 1
+        probs_mask[i][input_token_ids_lengths[i]:len(batch_token_ids[i])] = 1
 
     logits = model(padded_batch_token_ids, attention_mask=attention_mask).logits[:, min_input_length:]
     probs = torch.softmax(logits, dim = -1)
-    attention_mask = attention_mask[:, min_input_length:]
+    probs_mask = probs_mask[:, min_input_length:]
     padded_batch_token_ids = padded_batch_token_ids[:, min_input_length:]
 
-    # we need to improve the attention mask computation but it is already a good approximation
-    return torch.mean(torch.log(torch.gather(probs, dim=-1, index=padded_batch_token_ids.unsqueeze(-1)).squeeze(-1)) * attention_mask, dim=-1), indexes
+    return torch.mean(torch.log(torch.gather(probs, dim=-1, index=padded_batch_token_ids.unsqueeze(-1)).squeeze(-1)) * probs_mask, dim=-1), indexes
 
 def main(args):
     data = torch.load("datasets/extracted_anthropic_hh.pth")