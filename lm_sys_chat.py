dbd0f85a3e8db30ada7586c06249177065b89a5b
Antonio Davi Macedo Coelho de Castro <adavimacedo@gmail.com>
2024-06-04 23:44:54 -0700

    fix batch once more

--- lm_sys_chat.py
+++ lm_sys_chat.py
@@ -103,7 +103,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--small", action="store_true", help="if true, use gpt2, else, use gpt2-xl")
     parser.add_argument("-t", "--tokenize", action="store_true", help="tokenize texts")
-    parser.add_argument("-b", "--batch", type=int, default=320, help="the batch size")
+    parser.add_argument("-b", "--batch", type=int, default=512, help="the batch size")
     parser.add_argument("-a", "--amount", type=int, default=1000000, help="the amount of rows to extract")
 
     args = parser.parse_args()
