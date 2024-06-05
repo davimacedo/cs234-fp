b18ccebb7f099dd8249528d9060a6ed761e23585
Antonio Davi Macedo Coelho de Castro <adavimacedo@gmail.com>
2024-06-04 22:43:18 -0700

    fix bug in evaluate

--- evaluate.py
+++ evaluate.py
@@ -10,9 +10,9 @@ def main(args):
     input_texts = data["input_texts"]
     chosen_output_texts = data["chosen_output_texts"]
     rejected_output_texts = data["rejected_output_texts"]
-    input_token_ids = torch.tensor(data["input_token_ids"]) if input_token_ids in data else None
-    chosen_output_token_ids = torch.tensor(data["chosen_output_token_ids"]) if chosen_output_token_ids in data else None
-    rejected_output_token_ids = torch.tensor(data["rejected_output_token_ids"]) if rejected_output_token_ids in data else None
+    input_token_ids = torch.tensor(data["input_token_ids"]) if "input_token_ids" in data else None
+    chosen_output_token_ids = torch.tensor(data["chosen_output_token_ids"]) if "chosen_output_token_ids" in data else None
+    rejected_output_token_ids = torch.tensor(data["rejected_output_token_ids"]) if "rejected_output_token_ids" in data else None
 
     total = len(input_texts)
     print(f"loaded {total} samples")
