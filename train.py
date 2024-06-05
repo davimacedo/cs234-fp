1100756ced2ef7d187855b7308f087ce4808245d
Antonio Davi Macedo Coelho de Castro <adavimacedo@gmail.com>
2024-06-05 00:05:11 -0700

    train batch fize again

--- train.py
+++ train.py
@@ -81,7 +81,7 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--small", action="store_true", help="if true, use gpt2, else, use gpt2-xl")
-    parser.add_argument("-b", "--batch", type=int, default=4, help="the batch size")
+    parser.add_argument("-b", "--batch", type=int, default=2, help="the batch size")
     parser.add_argument("-l", "--lr", type=float, default=1e-6, help="the optimizer learning rate")
 
     args = parser.parse_args()
