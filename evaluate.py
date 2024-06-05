d5f173867da13c27b2c003d9db28d87a61394880
Antonio Davi Macedo Coelho de Castro <adavimacedo@gmail.com>
2024-06-04 22:50:55 -0700

    fix batch sizes

--- evaluate.py
+++ evaluate.py
@@ -73,7 +73,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-o", "--original", action="store_true", help="if true, use original model")
     parser.add_argument("-s", "--small", action="store_true", help="if true, use gpt2, else, use gpt2-xl")
-    parser.add_argument("-b", "--batch", type=int, default=512, help="the batch size")
+    parser.add_argument("-b", "--batch", type=int, default=32, help="the batch size")
 
     args = parser.parse_args()
 
--- lm_sys_chat.py
+++ lm_sys_chat.py
@@ -105,7 +105,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--small", action="store_true", help="if true, use gpt2, else, use gpt2-xl")
     parser.add_argument("-t", "--tokenize", action="store_true", help="tokenize texts")
-    parser.add_argument("-b", "--batch", type=int, default=512, help="the batch size")
+    parser.add_argument("-b", "--batch", type=int, default=32, help="the batch size")
 
     args = parser.parse_args()
 
