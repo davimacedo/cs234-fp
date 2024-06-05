a9b99fb5a869109137d756c92238462415de73ca
Antonio Davi Macedo Coelho de Castro <adavimacedo@gmail.com>
2024-06-05 05:25:41 -0700

    imrpove sentimento memory

--- sentiment.py
+++ sentiment.py
@@ -1,34 +1,46 @@
-from transformers import AutoTokenizer, pipeline
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, pipeline
 import torch
 from settings import device
 
 MODEL_PATH = "cardiffnlp/twitter-roberta-base-sentiment-latest"
 
-tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH, device=device)
-tokenizer.model_max_length = 512
-sentiment_pipeline = pipeline(
-    "sentiment-analysis",
-    model=MODEL_PATH,
-    tokenizer=tokenizer,
-    device=device,
-    truncation=True,
-    padding=True
-)
+sentiment_pipeline = None
+
+def initialize():
+    global sentiment_pipeline
+
+    if sentiment_pipeline is None:
+        print("initialing sentiment pipeline...")
+        model = AutoModelForSequenceClassification.from_pretrained(MODEL_PATH, device=device)
+        model.eval()
+        tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH, device=device)
+        tokenizer.model_max_length = 512
+        sentiment_pipeline = pipeline(
+            "sentiment-analysis",
+            model=model,
+            tokenizer=tokenizer,
+            device=device,
+            truncation=True,
+            padding=True
+        )
 
 def predict_sentiments(texts):
-    sentiments = sentiment_pipeline(texts)
+    initialize()
 
-    scores = []
+    with torch.no_grad():
+        sentiments = sentiment_pipeline(texts)
 
-    for sentiment in sentiments:
-        if sentiment["label"] == "positive":
-            scores.append(sentiment["score"])
-        elif sentiment["label"] == "negative":
-            scores.append(-sentiment["score"])
-        else:
-            scores.append(0)
+        scores = []
 
-    return torch.tensor(scores)
+        for sentiment in sentiments:
+            if sentiment["label"] == "positive":
+                scores.append(sentiment["score"])
+            elif sentiment["label"] == "negative":
+                scores.append(-sentiment["score"])
+            else:
+                scores.append(0)
+
+        return torch.tensor(scores)
 
 def main():
     sentiments = predict_sentiments([
